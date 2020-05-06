eed0b92c4ebed826c6f848174d48311f68f1a7b9
Deva Kumar <devakumar997@gmail.com>
2020-05-06 18:27:25 +0530

    Added debounce time

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Debounce } from 'react-throttle';
 
 import Book from './Book';
 
 class SearchPage extends Component {
-    state = { results : [] }
+    state = { results: [] }
 
     handleInputChange = e => {
         this.props.handleSearch(e.target.value)
-        .then(res => {
-            if(!res.error) {
-                res = res.map(resBook => {
-                    const existed = this.props.currentBooks.find(currentBook => currentBook.id === resBook.id);
-                    if(existed) return existed;
-                    return resBook;
-                });
-                this.setState({ results: res });
-            }     
-        })
-        .catch(err => console.log(err));
+            .then(res => {
+                if (!res.error) {
+                    res = res.map(resBook => {
+                        const existed = this.props.currentBooks.find(currentBook => currentBook.id === resBook.id);
+                        if (existed) return existed;
+                        return resBook;
+                    });
+                    this.setState({ results: res });
+                }
+            })
+            .catch(err => console.log(err));
     }
     render() {
         return (
@@ -26,7 +27,9 @@ class SearchPage extends Component {
                 <div className="search-books-bar">
                     <Link to="/" className="close-search">Close</Link>
                     <div className="search-books-input-wrapper">
-                        <input type="text" placeholder="Search by title or author" onChange={this.handleInputChange} />
+                        <Debounce time='400' handler="onChange">
+                            <input type="text" placeholder="Search by title or author" onChange={this.handleInputChange} />
+                        </Debounce>
                     </div>
                 </div>
                 <div className="search-books-results">
